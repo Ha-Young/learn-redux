5c4e4bee17d91c78a1772aeb3e6e1606bed65e4c
Ha0 <hayeong28@naver.com>
2020-06-27 00:49:33 +0900

    learn redux - createStore

---
+++ src/exercise.js
@@ -0,0 +1,64 @@
+import { createStore } from 'redux';
+
+const initialState = {
+  counter: 0,
+  text: '',
+  list: [],
+};
+
+const INCREASE = 'INCREASE';
+const DECREASE = 'DECREASE';
+const CHANGE_TEXT = 'CHANGE_TEXT';
+const ADD_TO_LIST = 'ADD_TO_LIST';
+
+const increase = () => ({
+  type: INCREASE,
+});
+
+const decrease = () => ({
+  type: DECREASE,
+});
+
+const changeText = (text) => ({
+  type: CHANGE_TEXT,
+  text,
+});
+
+const addToList = (item) => ({
+  type: ADD_TO_LIST,
+  item,
+});
+
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case INCREASE:
+      return {
+        ...state,
+        counter: state.counter + 1,
+      };
+
+    case DECREASE:
+      return {
+        ...state,
+        counter: state.counter - 1,
+      };
+
+    case CHANGE_TEXT:
+      return {
+        ...state,
+        text: action.text,
+      };
+
+    case ADD_TO_LIST:
+      return {
+        ...state,
+        list: state.list.concat(action.item),
+      };
+
+    default:
+      return state;
+  }
+}
+
+const store = createStore(reducer);
+console.log(store.getState());
